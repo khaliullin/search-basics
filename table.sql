e716dc433ce562156ca8541180d539cfe1252f99
khaliullin <sgtkaliullin@gmail.com>
2019-02-24 18:49:05 +0300

    Homework 1 update. UUID field added

--- table.sql
+++ table.sql
@@ -1,15 +1,17 @@
+CREATE EXTENSION "uuid-ossp";
+
 CREATE TABLE students (
-  id SERIAL PRIMARY KEY,
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
   name VARCHAR(32),
   surname VARCHAR(32),
   mygroup VARCHAR(32)
 );
 
 CREATE TABLE articles (
-  id SERIAL PRIMARY KEY,
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
   title VARCHAR(256),
   keywords VARCHAR(256),
   content TEXT,
   url VARCHAR(128),
-  student_id INT REFERENCES students(id)
+  student_id uuid REFERENCES students(id)
 );
